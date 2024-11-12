1349b45ada9d134916ed523c960bcadcdd2db0e8
sktanui <shadrackkemei2019@gmail.com>
2024-11-12 15:37:52 +0300

    components

---
+++ src/components/JobListing/JobListing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './JobListing.module.css';
+
+const JobListing = () => (
+  <div className={styles.JobListing}>
+    JobListing Component
+  </div>
+);
+
+JobListing.propTypes = {};
+
+JobListing.defaultProps = {};
+
+export default JobListing;
