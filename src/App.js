e3ed9d27870d677d34ff6b3d744aaa4e16d8009b
sktanui <shadrackkemei2019@gmail.com>
2024-11-13 00:07:47 +0300

    added signup component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, {useState} from "react"
 import LoginForm from "./components/LoginForm/LoginForm"
 import "./App.css"
+import SignupForm from "./components/SignupForm/SignupForm";
 
 function App() {
     const [user, setUser] = useState(null);
     const [isLoginVisible, setIsLoginVisible] = useState(false);
+    const [isSignupVisible, setIsSignupVisible] = useState(false);
 
     function handleLogin(loggedInUser) {
         setUser(loggedInUser)
         setIsLoginVisible(false) // close the login form after login
     }
 
+    function handleSignup(newUser) {
+        setUser(newUser)
+        setIsSignupVisible(false) // Close the signup form after signup
+    }
+
     function toggleLoginForm() {
         setIsLoginVisible(!isLoginVisible);
+    }
+
+    function toggleSignupForm() {
+        setIsSignupVisible(!isSignupVisible)
     }
 
   
@@ -22,13 +33,24 @@ function App() {
             {user ? (
                 <div>Welcome, {user.name}!</div>
             ) : (
+                <div>
               <button onClick={toggleLoginForm}>Login</button>
+              <button onClick={toggleSignupForm}>Sign Up</button>
+              </div>
             )}
            {isLoginVisible && (
             <div className="login-overlay">
                 <div className="login-modal">
                     <button className="close-button" onClick={toggleLoginForm}>x</button>
                     <LoginForm onLogin={handleLogin} />
+                </div>
+            </div>
+           )}
+           {isSignupVisible && (
+            <div className="overlay">
+                <div className="modal">
+                    <button className="close-button" onClick={toggleSignupForm}>x</button>
+                    <SignupForm onSignup={handleSignup}/>
                 </div>
             </div>
            )}
