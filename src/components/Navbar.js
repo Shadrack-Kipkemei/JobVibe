d8d977b9af17b4bc8c844b7ae3c755355d6c0d9f
ianimbuga <imbugaian20@gmail.com>
2024-11-13 12:08:36 +0300

    components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; 
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav id='nav'>
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
