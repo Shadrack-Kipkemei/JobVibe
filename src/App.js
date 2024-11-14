a5ea34a2e94b45eb1e175de7f7b7af173b37bca5
sktanui <shadrackkemei2019@gmail.com>
2024-11-14 20:23:36 +0300

    component changes

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+
+
+import React from 'react';
+import './App.css';
+import HomePage from './components/Homepage/Homepage';
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
+
